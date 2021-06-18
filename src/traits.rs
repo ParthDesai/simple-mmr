ddb06993ddff6463e0673ec38f72ab55b0f04bbe
Parth Desai <desaiparth08@gmail.com>
2021-06-18 16:22:46 +0530

    add helper functions + usize as Idx

--- src/traits.rs
+++ src/traits.rs
@@ -1,5 +1,25 @@
-use std::ops::{Add, Index, IndexMut, Sub};
+use std::ops::{Index, IndexMut};
 use std::slice::SliceIndex;
+
+pub fn right_sibling(pos: usize, height: u32) -> usize {
+    pos + (2usize.pow(height + 1) - 1)
+}
+
+pub fn left_sibling(pos: usize, height: u32) -> usize {
+    pos - (2usize.pow(height + 1) - 1)
+}
+
+pub fn peak(max_pos: usize) -> usize {
+    f64::log2((max_pos + 1) as f64) as usize - 1
+}
+
+pub fn left_child(pos: usize, height: u32) -> usize {
+    pos - (2usize.pow(height))
+}
+
+pub fn right_child(pos: usize) -> usize {
+    pos - 1
+}
 
 pub trait Hashable {
     type Output;
@@ -13,25 +33,21 @@ pub trait SimpleMMR<Data: Hashable, Error: std::error::Error> {
     fn root(&self) -> Data::Output;
 }
 
-pub trait SimpleMMRStorage<Id, O>: Index<Id, Output = O> + IndexMut<Id, Output = O> {
+pub trait SimpleMMRStorage<O>: Index<usize, Output = O> + IndexMut<usize, Output = O> {
     fn length(&self) -> usize;
     fn capacity(&self) -> usize;
     fn increase_capacity(&mut self, increase_by: usize) -> usize;
     fn decrease_capacity(&mut self, decrease_by: usize) -> usize;
 }
 
-impl<H, Err, Id, O> SimpleMMR<H, Err> for dyn SimpleMMRStorage<Id, O>
+impl<H, Err, O> SimpleMMR<H, Err> for dyn SimpleMMRStorage<O>
 where
-    Id: Eq + Ord + Add + Sub,
     H: Hashable<Output = O>,
     Err: std::error::Error,
 {
     fn append(&mut self, data: H) -> Result<(), Err> {
         let hash = data.hash();
         let current_length = self.length();
-        if current_length == self.capacity() {
-            self.increase_capacity(1);
-        }
         todo!()
     }
 
