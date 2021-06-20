e9ccb552cb0b64a46b6e4f27d04dc7369c7ab06a
Parth Desai <desaiparth08@gmail.com>
2021-06-20 18:35:39 +0530

    decomposing traits

--- src/traits.rs
+++ src/traits.rs
@@ -1,5 +1,4 @@
 use std::ops::{Index, IndexMut};
-use std::slice::SliceIndex;
 
 pub fn right_sibling(pos: usize, height: u32) -> usize {
     pos + (2usize.pow(height + 1) - 1)
@@ -22,40 +21,83 @@ pub fn right_child(pos: usize) -> usize {
 }
 
 pub trait Hashable {
-    type Output;
+    type Output: Default;
     fn hash(&self) -> Self::Output;
+}
+
+pub trait Output {
+    type Hash;
+
+    fn height(&self) -> u32;
+    fn hash(&self) -> Self::Hash;
+
+    fn new(height: u32, hash: Self::Hash) -> Self;
+}
+
+pub trait HeightCache {
+    fn number_of_elements_at(&self, height: u32) -> usize;
+    fn increment_elements_at(&mut self, height: u32);
+    fn decrease_element_at(&mut self, height: u32);
 }
 
 // Id: Eq + Ord + Add + Sub, T: Index<Id> + IndexMut<Id>
 pub trait SimpleMMR<Data: Hashable, Error: std::error::Error> {
     fn append(&mut self, data: Data) -> Result<(), Error>;
-    fn prune(&mut self) -> Result<(), Error>;
     fn root(&self) -> Data::Output;
 }
 
-pub trait SimpleMMRStorage<O>: Index<usize, Output = O> + IndexMut<usize, Output = O> {
+pub trait PrunableMMR<Data: Hashable, Error: std::error::Error>: SimpleMMR<Data, Error> {
+    fn prune(&mut self) -> Result<(), Error>;
+}
+
+pub trait SimpleMMRStorage<O>:
+    Index<usize, Output = O> + IndexMut<usize, Output = O> + HeightCache
+{
     fn length(&self) -> usize;
     fn capacity(&self) -> usize;
     fn increase_capacity(&mut self, increase_by: usize) -> usize;
-    fn decrease_capacity(&mut self, decrease_by: usize) -> usize;
 }
 
-impl<H, Err, O> SimpleMMR<H, Err> for dyn SimpleMMRStorage<O>
+pub trait PrunableSimpleMMRStorage<O>: SimpleMMRStorage<O> {
+    fn prune_element(&mut self, elem_index: usize);
+}
+
+impl<H, Err, O, Hash> SimpleMMR<H, Err> for dyn SimpleMMRStorage<O>
 where
-    H: Hashable<Output = O>,
+    Hash: Default,
+    O: Output<Hash = Hash>,
+    H: Hashable<Output = Hash>,
     Err: std::error::Error,
 {
     fn append(&mut self, data: H) -> Result<(), Err> {
-        let hash = data.hash();
-        let current_length = self.length();
-        todo!()
-    }
-
-    fn prune(&mut self) -> Result<(), Err> {
         todo!()
     }
 
     fn root(&self) -> H::Output {
+        todo!()
+    }
+}
+
+// Due to Rust's restriction on non-auto trait, we need to create placeholder trait composed of
+// PrunableSimpleMMRStorage + SimpleMMR to implement PrunableMMR on all objects that implement
+// PrunableSimpleMMRStorage and SimpleMMR
+pub trait PrunableStorageWithSimpleMMR<
+    Hash: Default,
+    O: Output<Hash = Hash>,
+    H: Hashable<Output = Hash>,
+    Error: std::error::Error,
+>: PrunableSimpleMMRStorage<O> + SimpleMMR<H, Error>
+{
+}
+
+impl<H, Err, O, Hash> PrunableMMR<H, Err> for dyn PrunableStorageWithSimpleMMR<Hash, O, H, Err>
+where
+    Hash: Default,
+    O: Output<Hash = Hash>,
+    H: Hashable<Output = Hash>,
+    Err: std::error::Error,
+{
+    fn prune(&mut self) -> Result<(), Err> {
         todo!()
     }
 }
